038118559ff9d3e38d7b4160b0e3236f558912c4
karollukawski <83611706+karollukawski@users.noreply.github.com>
2022-06-23 21:35:27 +0200

    Update v1

--- spendings_tracker.py
+++ spendings_tracker.py
@@ -8,11 +8,35 @@ while True:
     if month > 12:
         break
 
+    expenses = []
+
+    def show_expenses (month):
+        print (expenses)
+
+    def add_expense(month):
+        print ()
+        expense_amount = int(input("Give an amout [$]: "))
+        expense_type = input ("Give a type of expense [food, entertainment, home, other]: ")
+
+        expense = (expense_amount, expense_type, month)
+        expenses.append(expense)
+
     while True:
         print ()
-        print ("Choose option: ")
+        print ("Available options: ")
         print ("0 - Return to the selection of the month")
         print ("1 - Show all spendings")
         print ("2 - Add a new expense")
+
+        choice = int(input("Choose option: "))
+
+        if choice == 0:
+            break
+        if choice == 1:
+            show_expenses(month)
+        if choice == 2:
+            add_expense(month)
+        else:
+            break
 
     
