7cd0dc7061a1cd6a35e7d992c71ddca9fffc635e
karollukawski <83611706+karollukawski@users.noreply.github.com>
2022-06-23 22:41:12 +0200

    Update v3

--- spendings_tracker.py
+++ spendings_tracker.py
@@ -13,7 +13,7 @@ while True:
     def show_expenses (month):
         for expense_amount, expense_type, expense_month in expenses:
             if expense_month == month:
-                print (f'{expense_amount}$ - {expense_type}')
+                print (f'{expense_amount} - {expense_type}')
 
     def add_expense(month):
         print ()
@@ -23,12 +23,24 @@ while True:
         expense = (expense_amount, expense_type, month)
         expenses.append(expense)
 
+    def show_stats(month):
+        
+        total_amount_this_month = sum(expense_amount for expense_amount, expense_month in expenses if expense_month == month)
+        total_amount_all_months = sum(expense_amount for expense_amount in expenses)
+        
+
+        print ()
+        print("Stats")
+        print ("All spendings in this month [$]: ", total_amount_this_month)
+        print ("All spendings [$]: ", total_amount_all_months)
+
     while True:
         print ()
         print ("Available options: ")
         print ("0 - Return to the selection of the month")
         print ("1 - Show all spendings")
         print ("2 - Add a new expense")
+        print ("3 - Stats")
 
         choice = int(input("Choose option: "))
 
@@ -38,6 +50,8 @@ while True:
             show_expenses(month)
         if choice == 2:
             add_expense(month)
+        if choice == 3:
+            show_stats(month)
         else:
             break
 
