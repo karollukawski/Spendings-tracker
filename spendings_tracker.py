dddb235fece79a665d56bda5000f635f47114ea6
karollukawski <83611706+karollukawski@users.noreply.github.com>
2022-06-24 14:59:44 +0200

    Update v5

--- spendings_tracker.py
+++ spendings_tracker.py
@@ -1,3 +1,35 @@
+expenses = []
+
+def show_expenses (month):
+    for expense_amount, expense_type, expense_month in expenses:
+        if expense_month == month:
+            print (f'{expense_amount} - {expense_type}')
+
+def add_expense(month):
+    print ()
+    expense_amount = int(input("Give an amout [$]: "))
+    expense_type = input ("Give a type of expense [food, entertainment, home, other]: ")
+
+    expense = (expense_amount, expense_type, month)
+    expenses.append(expense)
+
+def show_stats(month):
+        
+    total_amount_this_month = sum(expense_amount for expense_amount, expense_month in expenses if expense_month == month)
+    number_of_expenses_this_month = sum(1 for expense_month in expenses if expense_month == month)
+    average_expense_this_month = total_amount_this_month / number_of_expenses_this_month
+
+    total_amount_all_months = sum(expense_amount for expense_amount in expenses)
+    average_expense_all_months = total_amount_all_months / len(expenses)
+        
+
+    print ()
+    print("Stats")
+    print ("All spendings in this month [$]: ", total_amount_this_month)
+    print ("Average expense in this month [$]: ", average_expense_this_month)
+    print ("All spendings [$]: ", total_amount_all_months)
+    print ("Average expense [$]: ", average_expense_all_months)
+
 while True:
     print ()
     month = int(input ("Choose a month (1-12): "))
@@ -8,37 +40,7 @@ while True:
     if month > 12:
         break
 
-    expenses = []
-
-    def show_expenses (month):
-        for expense_amount, expense_type, expense_month in expenses:
-            if expense_month == month:
-                print (f'{expense_amount} - {expense_type}')
-
-    def add_expense(month):
-        print ()
-        expense_amount = int(input("Give an amout [$]: "))
-        expense_type = input ("Give a type of expense [food, entertainment, home, other]: ")
-
-        expense = (expense_amount, expense_type, month)
-        expenses.append(expense)
-
-    def show_stats(month):
-        
-        total_amount_this_month = sum(expense_amount for expense_amount, expense_month in expenses if expense_month == month)
-        number_of_expenses_this_month = sum(1 for expense_month in expenses if expense_month == month)
-        total_amount_all_months = sum(expense_amount for expense_amount in expenses)
-        average_expense_this_month = total_amount_this_month / number_of_expenses_this_month
-        average_expense_all_months = total_amount_all_months / len(expenses)
-        
-
-        print ()
-        print("Stats")
-        print ("All spendings in this month [$]: ", total_amount_this_month)
-        print ("Average expense in this month [$]: ", average_expense_this_month)
-        print ("All spendings [$]: ", total_amount_all_months)
-        print ("Average expense [$]: ", average_expense_all_months)
-
+    
     while True:
         print ()
         print ("Available options: ")
